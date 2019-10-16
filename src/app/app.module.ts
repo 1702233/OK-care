35080b1ff1972211c5a1f9bc20d672df152d501c
Gideon <1702233@student.hu.nl>
2019-10-16 14:05:40 +0200

    setup mappen structuur en basis componenten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+//config
+import { firebaseConfig } from '../environments/firebase.config';
+//components
+import { LoginComponent } from './modules/components/login/login.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
