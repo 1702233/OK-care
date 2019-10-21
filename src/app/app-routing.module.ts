ea08fd0863f4d6ecbcc435e5eeb4ee4181ba50e4
aronvvijv <aronruizendaal@gmail.com>
2019-10-21 12:33:52 +0200

    added auth to menubar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'operaties', component: AddOperatieComponent},
+  { path: 'operaties', component: AddOperatieComponent, canActivate: [AuthGuard] },
   {
     path: '**',
     redirectTo: 'login',
